bfa16e1aeeb7b8df2667de9de502261982a7cf21
MirceaRF <107954018+MirceaRF@users.noreply.github.com>
2022-09-15 21:05:07 +0300

    Update Suites_Test.py

--- Unitest_Final_Project/Suites_Test.py
+++ Unitest_Final_Project/Suites_Test.py
@@ -6,19 +6,19 @@ from login_test import Login
 from product import Product
 
 
-class TestSuite(unittest.TestCase):           # pentru ca am importat toata libraria este nevoie sa specificam in fata clasei TestCase
+class TestSuite(unittest.TestCase):              # pentru ca am importat toata libraria este nevoie sa specificam in fata clasei TestCase
 
-                                               # libraria din care sa fie extrasa
-                                               # Daca importam doar libraria, sistemul va avea doar adresa de identificare a librariei,
-                                                # nu si a clasei TestCase
-                                               # Suita de teste = un set de teste care pot fi rulate in acelasi timp
-    def test_suite(self):                  # numele metodei este predefinit si NU trebuie schimbat
-        teste_de_rulat = unittest.TestSuite()  # am creat un obiect al clasei TestSuite
-        # prin obiectul teste_de_rulat vom accesa metoda addTests
-        # metoda addTests primeste ca si parametru o lista de teste care se doreste a fi executate
-        # lista de teste va fi separata prin virgula
+                                                 # libraria din care sa fie extrasa
+                                                 # Daca importam doar libraria, sistemul va avea doar adresa de identificare a librariei,
+                                                 # nu si a clasei TestCase
+                                                 # Suita de teste = un set de teste care pot fi rulate in acelasi timp
+    def test_suite(self):                        # numele metodei este predefinit si NU trebuie schimbat
+        teste_de_rulat = unittest.TestSuite()    # am creat un obiect al clasei TestSuite
+                                                 # prin obiectul teste_de_rulat vom accesa metoda addTests
+                                                 # metoda addTests primeste ca si parametru o lista de teste care se doreste a fi executate
+                                                 # lista de teste va fi separata prin virgula
 
-        # teste_de_rulat.addTests([]) metoda add tests, apelata fara parametru
+                                                 # teste_de_rulat.addTests([]) metoda add tests, apelata fara parametru
 
         teste_de_rulat.addTests([
             unittest.defaultTestLoader.loadTestsFromTestCase(Login),
